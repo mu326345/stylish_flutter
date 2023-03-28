5eab49d3ae4002af9746e290e3c2612d5166a320
yuyu <mu326345@gmail.com>
2023-03-29 01:52:39 +0800

    week1 230329

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,5 @@
-import 'package:flutter/material.dart'
-    show
-        AppBar,
-        AspectRatio,
-        Axis,
-        BorderRadius,
-        BoxFit,
-        BuildContext,
-        Card,
-        ClipRRect,
-        Color,
-        Colors,
-        Column,
-        Container,
-        EdgeInsets,
-        Expanded,
-        Image,
-        ListView,
-        MaterialApp,
-        MediaQuery,
-        NeverScrollableScrollPhysics,
-        Padding,
-        RoundedRectangleBorder,
-        Scaffold,
-        ScrollPhysics,
-        SingleChildScrollView,
-        SizedBox,
-        StatelessWidget,
-        Text,
-        Widget,
-        runApp;
+import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:flutter/src/widgets/basic.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 void main() => runApp(const MyApp());
 
@@ -40,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter layout demo',
+      // title: 'Flutter layout demo',
       home: Scaffold(
           appBar: AppBar(
             title: Image.asset(
@@ -48,85 +16,9 @@ class MyApp extends StatelessWidget {
               fit: BoxFit.fitHeight,
               height: 20,
             ),
-            backgroundColor: Color(0xFFEEF1F7),
+            backgroundColor: const Color(0xFFEEF1F7),
           ),
           body: HomePage()),
-    );
-  }
-}
-
-class BuildCard extends StatelessWidget {
-  const BuildCard({
-    super.key,
-  });
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 300,
-      height: 250,
-      padding: EdgeInsets.all(8),
-      child: Card(
-        color: Colors.deepOrange,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20),
-        ),
-      ),
-    );
-  }
-}
-
-class BuildCard1 extends StatelessWidget {
-  const BuildCard1({
-    super.key,
-  });
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 300,
-      height: 250,
-      padding: EdgeInsets.all(8),
-      child: Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Container(
-              width: 120,
-              height: double.infinity,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(20),
-                  bottomLeft: Radius.circular(20),
-                ),
-                color: Colors.deepOrange,
-              ),
-            ),
-            Expanded(
-                child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'UNIQLO IS GOOD',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Text(
-                  'NT\$ 343',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
-                )
-              ],
-            ))
-          ],
-        ),
-      ),
     );
   }
 }
@@ -143,88 +35,135 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    var buildCard = BuildCard();
-    return Container(
-        child: Column(
+    return Column(
       children: [
         Expanded(
-          flex: 4,
-          child: ListView.separated(
-            padding: EdgeInsets.fromLTRB(10, 20, 10, 20),
-            scrollDirection: Axis.horizontal,
-            itemCount: 6,
-            separatorBuilder: (context, _) => SizedBox(
-              width: 20,
-            ),
-            itemBuilder: (context, index) => buildCard,
-          ),
+          flex: 3,
+          child: topHoriListView(),
         ),
         Expanded(
-          flex: 6,
-          child: isSmall(context)
-              ? SingleChildScrollView(
-                  physics: ScrollPhysics(),
-                  child: SizedBox(
-                    height: MediaQuery.of(context).size.height / 2,
-                    child: Column(
-                      children: <Widget>[
-                        buildColumn('女裝'),
-                        buildColumn('男裝'),
-                        buildColumn('配件'),
-                      ],
-                    ),
+            flex: 7,
+            child: Column(
+              children: <Widget>[
+                ExpansionTile(
+                  title: Text(
+                    '女裝',
                   ),
-                )
-              : Row(
                   children: [
-                    buildColumn('女裝'),
-                    buildColumn('男裝'),
-                    buildColumn('配件'),
+                    SizedBox(
+                      height: double.maxFinite,
+                      child: productVertiListView(),
+                    )
                   ],
-                ),
-        )
+                )
+              ],
+            ))
       ],
-    ));
+    );
   }
 
-  Widget buildListView() {
-    var buildCard = BuildCard();
-    return ListView.separated(
-      padding: EdgeInsets.fromLTRB(10, 20, 10, 20),
+
+  // Column(children: [
+            //   Container(child: categoryVertiListView('女裝'),),
+            //   Container(child: categoryVertiListView('男裝')),
+            //   Container(child: categoryVertiListView('配件')),
+            // ],)
+            
+
+
+//     //這是大畫面時的view
+            //     Row(children: [
+            //   Expanded(child: categoryVertiListView('女裝')),
+            //   Expanded(child: categoryVertiListView('男裝')),
+            //   Expanded(child: categoryVertiListView('配件')),
+            // ]),
+
+            
+
+// 上方水平畫面元件區
+  Widget topImageCard() {
+    return SizedBox(
+      width: 300,
+      height: 250,
+      child: Card(
+        color: Colors.deepOrange,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Image.asset(
+          'images/Image_Logo02.png',
+          fit: BoxFit.contain,
+        ),
+      ),
+    );
+  }
+
+  Widget topHoriListView() {
+    return ListView.builder(
       scrollDirection: Axis.horizontal,
       itemCount: 6,
-      separatorBuilder: (context, _) => SizedBox(
-        width: 20,
-      ),
-      itemBuilder: (context, index) => buildCard,
+      itemBuilder: (context, index) => topImageCard(),
+      padding: EdgeInsets.all(5),
     );
   }
 
-  Widget buildColumn(String title) {
-    var buildCar1 = BuildCard1();
-    return Expanded(
-      child: Column(
-        children: [
-          Text(
-            title,
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+//下方畫面元件區
+  Widget productCard() {
+    return Container(
+      width: 300,
+      height: 180,
+      child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
           ),
-          SizedBox(height: 10),
-          Expanded(
-              child: SingleChildScrollView(
-            child: ListView.separated(
-              scrollDirection: Axis.vertical,
-              itemBuilder: (context, index) => buildCar1,
-              separatorBuilder: (context, _) => SizedBox(
-                height: 20,
-              ),
-              itemCount: 6,
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
-            ),
+          child: Row(
+            children: [
+              Expanded(
+                  flex: 4,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        bottomLeft: Radius.circular(20),
+                      ),
+                    ),
+                  )),
+              Expanded(
+                  flex: 6,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [Text('Uniqlo is good'), Text('NT\$ 343')],
+                    ),
+                  ))
+            ],
           )),
-        ],
-      ),
     );
+  }
+
+  Widget productVertiListView() {
+    return ListView.builder(
+      scrollDirection: Axis.vertical,
+      physics: const NeverScrollableScrollPhysics(),
+      itemCount: 6,
+      itemBuilder: (context, index) => productCard(),
+      padding: const EdgeInsets.all(5),
+    );
+  }
+
+  Widget categoryVertiListView(String category) {
+    return Column(children: [
+      Text(category),
+      Expanded(
+          child: ListView.builder(
+        scrollDirection: Axis.vertical,
+        itemCount: 6,
+        itemBuilder: (context, index) => productCard(),
+        padding: const EdgeInsets.all(5),
+      ))
+    ]);
   }
 }
