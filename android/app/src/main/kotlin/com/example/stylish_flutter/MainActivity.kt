d84ed8080b916520081caae0e646668193582878
yuyu <mu326345@gmail.com>
2023-05-02 00:55:33 +0800

    完成串接tappay

--- android/app/src/main/kotlin/com/example/stylish_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/stylish_flutter/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.stylish_flutter
 
+import android.app.AlertDialog
+import android.content.ContextWrapper
 import android.util.Log
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
@@ -12,21 +14,25 @@ class MainActivity: FlutterActivity() {
         super.configureFlutterEngine(flutterEngine)
         MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
             call, result ->
-            if (call.method == "getPlatformData") {
-                val dataStr = getPlatformData()
+            if (call.method == "tappay") {
+                Log.d("QAQ", "go tappay")
+//                val contextWrapper = ContextWrapper(context)
+//                contextWrapper.setTheme(R.style.PaymentColorTheme)
+                val dialog = PrimeDialog(context, object : PrimeDialog.PrimeDialogListener {
+                    override fun onSuccess(prime: String) {
+                        Log.v("QAQ", "onSuccess, prime=$prime")
+                        result.success(prime)
+                    }
 
-                if (dataStr.isNotBlank()) {
-                    result.success(dataStr)
-                } else {
-                    result.error("UNAVAILABLE", "android data fail!", null)
-                }
+                    override fun onFailure(error: String) {
+                        Log.v("QAQ", "onFailure, error=$error")
+                        result.success(error)
+                    }
+                })
+                dialog.show()
             } else {
-                result.notImplemented()
+                result.error("404", "404", null)
             }
         }
-    }
-
-    private fun getPlatformData(): String {
-        return "this is from android string"
     }
 }
--- lib/pay/pay_page.dart
+++ lib/pay/pay_page.dart
@@ -15,19 +15,56 @@ class _PayPageState extends State<PayPage> {
   static const platform = MethodChannel('com.example.stylish_flutter/getPlatformData');
   String _dataStr = 'get nothing yet';
 
-  @override
-  void initState() {
-    super.initState();
-    _getPlatformData();
-  }
+//Tappay
+  String _tappayMessage = 'init';
+  int _counter = 0;
 
   @override
   Widget build(BuildContext context) {
-    // return Placeholder();
-    return Container(
-      child: Text(_dataStr),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Image.asset(
+                'images/Image_Logo02.png',
+                fit: BoxFit.fitHeight,
+                height: 20,
+              ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              _tappayMessage,
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _inputCreditCard,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
-    throw UnimplementedError();
+  }
+
+  Future<void> _inputCreditCard() async {
+    String message;
+
+    try {
+      final String result = await platform.invokeMethod('tappay');
+      message = result;
+    } on PlatformException catch (e) {
+      message = e.message ?? '';
+    }
+
+    setState(() {
+      _tappayMessage = message;
+    });
   }
 
   Future<void> _getPlatformData() async {
